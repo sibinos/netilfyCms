7e21a443cdf6c126f1e165bd7a2e7a093aaabffc
sibinos <sibin.os.g@gmail.com>
2022-11-08 11:19:15 +0530

    chanesover

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from "react-router-dom"
 import Home from "./pages/home"
+import Post from "./pages/post"
+
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
+      <Route exact path="/post/:id" render={props => <Post {...props} />} />
+
     </Router>
 
     
